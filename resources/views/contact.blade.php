3ce826b5e3d3b9416c5c09ecb53200e231dcdc25
gadajatah <dreamerrr20@gmail.com>
2022-05-27 12:09:54 +0700

    firt push

---
+++ resources/views/contact.blade.php
@@ -0,0 +1,8 @@
+Ini adalah halaman contact
+<p>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Cupiditate, dolores. Necessitatibus, itaque quibusdam? Iusto, libero non neque necessitatibus corporis temporibus quas totam amet vitae at rerum, delectus cupiditate odit praesentium!
+    Quibusdam doloremque repellat alias mollitia numquam laboriosam quos. Suscipit maxime autem est natus vel at debitis provident dicta nisi, in omnis consequuntur et ea expedita pariatur non quasi corrupti voluptate!
+    Obcaecati fugit iusto ipsa eaque, ipsam error. Aut magnam in ad aliquid temporibus culpa, officia sint illum sed ipsam fugit fugiat consequatur voluptatibus accusamus quam atque veniam odio dicta labore.
+    Cumque qui eveniet autem blanditiis voluptatibus numquam est fuga alias tempore, facilis quo recusandae nihil ducimus quos sed porro exercitationem dignissimos eaque reiciendis repellendus deleniti minima tenetur cupiditate. Est, animi.
+    Quo iusto nam exercitationem quam ratione, vel cumque reiciendis adipisci voluptate tenetur officiis hic asperiores ea fugiat sapiente quidem minima alias unde fuga dolorem similique. Expedita quidem fugit autem veniam?
+</p>
